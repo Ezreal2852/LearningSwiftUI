e75d95453bf485db67ba88712024bc8c9958de8e
Ezreal <544881532@qq.com>
2020-07-20 19:24:30 +0800

    Add files via upload

---
+++ Learning SwiftUI/Learning SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning SwiftUI
+//
+//  Created by Ezreal on 2020/7/16.
+//  Copyright © 2020 Ezeal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
